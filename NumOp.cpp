345e20422e8c4392d4c11ac54eb7251e1b89174f
juergendoellner <juergen.doellner@hpi.uni-potsdam.de>
2013-07-28 13:41:51 +0200

    init-1

---
+++ NumOp.cpp
@@ -0,0 +1,12 @@
+//
+//  NumOp.cpp
+//  NumLib
+//
+//  Created by Juergen Doellner on 28.07.13.
+//  Copyright (c) 2013 Juergen Doellner. All rights reserved.
+//
+
+#include "NumOp.h"
+
+NumOp::NumOp() {
+}
---
+++ NumOp.h
@@ -0,0 +1,19 @@
+//
+//  NumOp.h
+//  NumLib
+//
+//  Created by Juergen Doellner on 28.07.13.
+//  Copyright (c) 2013 Juergen Doellner. All rights reserved.
+//
+
+#ifndef __NumLib__NumOp__
+#define __NumLib__NumOp__
+
+#include <iostream>
+
+class NumOp {
+public:
+    NumOp();
+};
+
+#endif /* defined(__NumLib__NumOp__) */
